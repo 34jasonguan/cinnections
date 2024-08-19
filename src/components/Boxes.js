a219dfdd600c1a4c2521678814a1726fef32f9df
34jasonguan <34jasonguan@gmail.com>
2024-08-18 20:32:53 -0700

    setting up components

---
+++ src/components/Boxes.js
@@ -0,0 +1,10 @@
+import { useState } from "react";
+
+function Boxes(){
+    const [movies, setMovies] = useState([
+        { id: 1, img: '', stat: "" }
+    ])
+    return <p>Boxes</p>
+}
+
+export default Boxes; 
